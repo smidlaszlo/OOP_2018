f4dbbdda012c7a8f451cd7f15c2fc54e8195efba
smidlaszlo <smid@iit.uni-miskolc.hu>
2018-03-14 17:52:49 +0100

    bovites konstruktorral es cimEgyezikE metodussal

--- Konyv/src/Konyv.java
+++ Konyv/src/Konyv.java
@@ -1,3 +1,6 @@
+import java.util.*;
+import java.time.LocalDate;
+
 /*
 Készítsen Könyv osztályt és egy Könyv osztályt használó osztályt!
 a) A Könyv osztály adattagjai: cím, szerző, megjelenés éve, ára.
@@ -15,6 +18,38 @@ public class Konyv {
 	private int megjelenes_eve;
 	private int ar;
 
+	//Egészítse ki a Könyv osztályt 2 konstruktorral!
+	//Az egyik konstruktornak a könyv minden adatát meg kell adni,
+	//a másiknak csak a szerzőt és a címet,
+	//az ár 2500 Ft, a megjelenés éve pedig az aktuális év. 
+	public Konyv(String cim, String szerzo, int megjelenes_eve, int ar) {
+		this.cim = cim;
+		this.szerzo = szerzo;
+		this.megjelenes_eve = megjelenes_eve;
+		this.ar = ar;
+	}	
+
+	public Konyv(String cim, String szerzo) {
+		//Java 6, 7
+		//this(cim, szerzo, 1900 + new Date().getYear(), 2500);
+		//this(cim, szerzo, Calendar.getInstance().get(Calendar.YEAR), 2500);
+
+		//Java 8
+		this(cim, szerzo, LocalDate.now().getYear(), 2500);
+		
+		//Date mai_nap = new Date();		
+		//this.megjelenes_eve = mai_nap.getYear();
+		
+		//Calendar kalendarium = java.util.Calendar.getInstance();
+		//kalendarium.setTime(mai_nap);
+		//this.megjelenes_eve = kalendarium.get(Calendar.YEAR);
+		
+		//LocalDate aktualis_datum = java.time.LocalDate.now();
+		//this.megjelenes_eve = aktualis_datum.getYear();
+		
+		//this.ar = 2500;
+	}	
+	
 	public void arat_szazalekkal_novel(int szazalek) {
 		ar *= (1 + szazalek / 100.0);
 	}
@@ -54,5 +89,11 @@ public class Konyv {
 
 	public void setAr(int ar) {
 		this.ar = ar;
+	}
+	
+	//Írjon metódust, amely igazat ad, ha a könyv címe megegyezik
+	//a paraméterként kapott könyv címével, hamisat ha nem!
+	public boolean cimEgyezikE(Konyv masikKonyv) {
+		return cim.equals(masikKonyv.cim);
 	}
 }
--- Konyv/src/Konyvtar.java
+++ Konyv/src/Konyvtar.java
@@ -1,14 +1,9 @@
+import java.util.Scanner;
 
 public class Konyvtar {
 	public static void main(String[] args) {
 		//hozzunk letre egy konyv objektumot
-		Konyv konyv = new Konyv();
-		
-		//allitsuk be az adattagjait
-		konyv.setSzerzo("Kiss Kalman");
-		konyv.setCim("Kalman konyvei");
-		konyv.setMegjelenes_eve(2018);
-		konyv.setAr(2000);
+		Konyv konyv = new Konyv("Kalman konyvei", "Kiss Kalman", 2018, 2000);
 		
 		//irassuk ki a konyv adatait
 		System.out.println("A konyv adatai");
@@ -17,5 +12,49 @@ public class Konyvtar {
 		//noveljuk a kony arat 3%-kal
 		konyv.arat_szazalekkal_novel(3);		
 		System.out.println(konyv);		
+
+		//hasznaljuk a 2 paramteres konstruktort
+		Konyv masik_konyv = new Konyv("Nora novenyei", "Nagy Nora");
+		System.out.println(masik_konyv);
+		
+		//Írjon futtatható osztályt, amelyben létrehoz 2 könyvet
+		//(az adatokat beolvasva, mindkét konstruktort használva),
+		//majd kiírja a String-be összefûzött adataikat!
+		//Hasonlítsa össze a két könyvet, hogy megegyezik-e a címük!
+		Scanner beolvasas = new Scanner(System.in);
+		
+		System.out.println("Kerem adja meg az 1. konyv szerzojet!\nszerzo:");
+		String szerzo = beolvasas.nextLine();
+
+		System.out.println("Kerem adja meg az 1. konyv cimet!\ncim:");
+		String cim = beolvasas.nextLine();
+
+		System.out.println("Kerem adja meg az 1. konyv megjelenesi evet!\nmegjelenesi ev:");
+		int megjelenesi_ev = beolvasas.nextInt();
+		
+		System.out.println("Kerem adja meg az 1. konyv arat!\nar:");
+		int ar = beolvasas.nextInt();
+		
+		Konyv elso_konyv = new Konyv(cim, szerzo, megjelenesi_ev, ar);
+		
+		System.out.println("Az elso konyv adatai:");
+		System.out.println(elso_konyv);
+		
+		System.out.println("Kerem adja meg a 2. konyv szerzojet!\nszerzo:");
+		szerzo = beolvasas.nextLine(); szerzo = beolvasas.nextLine();
+
+		System.out.println("Kerem adja meg a 2. konyv cimet!\ncim:");
+		cim = beolvasas.nextLine();
+		
+		Konyv masodik_konyv = new Konyv(cim, szerzo);
+		
+		System.out.println("A masodik konyv adatai:");
+		System.out.println(masodik_konyv);
+		
+		System.out.println("Az utolso 2 konyv cime megegyezik-e?");
+		System.out.println(elso_konyv.cimEgyezikE(masodik_konyv));
+		
+		beolvasas.close();
+
 	}
 }
