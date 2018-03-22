b08c2470a90e08581e5121323bab27ad21af268d
smidlaszlo <smid@iit.uni-miskolc.hu>
2018-03-22 11:08:33 +0100

    Konyv egy csomagban

---
+++ Konyv/src/sajat/Konyv.java
@@ -0,0 +1,148 @@
+/*
+Készítsen Könyv osztályt és egy Könyv osztályt használó osztályt!
+a) A Könyv osztály adattagjai: cím, szerző, megjelenés éve, ára.
+Legyen egy metódusa, amely a paraméterként kapott
+százalékos értékkel növeli a könyv árát!
+Legyen egy metódusa, ami egy String-be összefűzi az adatokat
+és ezt adja vissza!
+b) Írjunk setter metódusokat az adatok beállításához,
+és getter metódusokat az adatok lekérdezéséhez
+(adatrejtés → ellenőrzött adathozzáférés). 
+ */
+
+/* Tag 1Konstruktor
+	//Egészítse ki a Könyv osztályt 2 konstruktorral!
+	//Az egyik konstruktornak a könyv minden adatát meg kell adni,
+	//a másiknak csak a szerzőt és a címet,
+	//az ár 2500 Ft, a megjelenés éve pedig az aktuális év. 
+ */
+
+/* Tag 2Csomag
+A Könyv osztályt helyezze át saját csomagba!
+- A szerző, cím és ár adattagok változatlanok.
+- Egészítse ki egy oldalszám adattaggal
+és egy kiadó osztályszintű adattaggal, melynek értéke “Móra”!
+- A megjelenés éve adattag legyen konstans, az objektum létrehozásának éve!
+- Módosítsa a 2 konstruktort ennek megfelelően!
+- Módosítsa azt a metódust, amely String-ben összefűzve adja vissza a könyv adatait!
+- Írjon metódust, amely két paraméterként kapott könyv közül a hosszabbat adja vissza!
+- Írjon metódust, amely igazat ad vissza, ha a könyv oldalszáma páros!
+ */
+package sajat;
+
+import java.util.*;
+import java.time.LocalDate;
+
+public class Konyv {
+	private String cim;
+	private String szerzo;
+	//modositas konstanssa, final modosito hozzaadasa
+	private final int megjelenes_eve;
+	private int ar;
+	private int oldalszam;
+	//osztalyszintu adattag
+	private static String kiado = "Mora Kiado";
+
+	public Konyv(String cim, String szerzo, int ar, int oldalszam) {
+		this.cim = cim;
+		this.szerzo = szerzo;
+		//this.megjelenes_eve = megjelenes_eve;
+		this.megjelenes_eve = LocalDate.now().getYear();
+		this.ar = ar;
+		this.oldalszam = oldalszam;
+	}	
+
+	public Konyv(String cim, String szerzo, int oldalszam) {
+		//Java 6, 7
+		//this(cim, szerzo, 1900 + new Date().getYear(), 2500);
+		//this(cim, szerzo, Calendar.getInstance().get(Calendar.YEAR), 2500);
+
+		//Java 8
+		//this(cim, szerzo, LocalDate.now().getYear(), 2500);
+		//this(cim, szerzo, LocalDate.now().getYear(), 2500);
+		this(cim, szerzo, 2500, oldalszam);
+		
+		//Date mai_nap = new Date();		
+		//this.megjelenes_eve = mai_nap.getYear();
+		
+		//Calendar kalendarium = java.util.Calendar.getInstance();
+		//kalendarium.setTime(mai_nap);
+		//this.megjelenes_eve = kalendarium.get(Calendar.YEAR);
+		
+		//LocalDate aktualis_datum = java.time.LocalDate.now();
+		//this.megjelenes_eve = aktualis_datum.getYear();
+		
+		//this.ar = 2500;
+	}	
+	
+	public void arat_szazalekkal_novel(int szazalek) {
+		ar *= (1 + szazalek / 100.0);
+	}
+	
+	@Override
+	public String toString() {
+		return szerzo + ": " + cim 
+				+ " (" 
+				+ kiado 
+				+ ", " 
+				+ megjelenes_eve 
+				+ ", " 
+				+ oldalszam
+				+ " old., "
+				+ ar 
+				+ " Ft)";
+	}
+
+	public String getCim() {
+		return cim;
+	}
+
+//	public void setCim(String cim) {
+//		this.cim = cim;
+//	}
+
+	public String getSzerzo() {
+		return szerzo;
+	}
+
+//	public void setSzerzo(String szerzo) {
+//		this.szerzo = szerzo;
+//	}
+
+	public int getMegjelenes_eve() {
+		return megjelenes_eve;
+	}
+
+//	public void setMegjelenes_eve(int megjelenes_eve) {
+//		this.megjelenes_eve = megjelenes_eve;
+//	}
+
+	public int getAr() {
+		return ar;
+	}
+
+	public void setAr(int ar) {
+		this.ar = ar;
+	}
+	
+	//Írjon metódust, amely igazat ad, ha a könyv címe megegyezik
+	//a paraméterként kapott könyv címével, hamisat ha nem!
+	public boolean cimEgyezikE(Konyv masikKonyv) {
+		return cim.equals(masikKonyv.cim);
+	}
+	
+	//Írjon metódust, amely két paraméterként kapott könyv közül a hosszabbat adja vissza!
+	public static Konyv hosszabbKonyv(Konyv egyik_konyv, Konyv masik_konyv) {
+		if (egyik_konyv.oldalszam > masik_konyv.oldalszam) {
+			return egyik_konyv;
+		} else {
+			return masik_konyv;
+		}
+	}
+	
+	//Írjon metódust, amely igazat ad vissza, ha a könyv oldalszáma páros!
+	public boolean oldalszamParosE() {
+		return oldalszam % 2 == 0;
+	}
+
+}
---
+++ Konyv/src/sajat/Konyvtar.java
@@ -0,0 +1,106 @@
+/* Tag 2Csomag
+Ugyanebben a csomagban készítsen egy futtatható osztályt,
+amelyben beolvas n darab könyvet egy tömbbe!
+- Írja ki a leghosszabb könyv adatait!
+- Írja ki a leghosszabb páros oldalszámú könyv adatait!
+- Írja ki, hogy melyik szerzõnek hány darab könyve jelent meg!
+ */
+
+package sajat;
+
+//import java.util.Random;
+//import java.util.Scanner;
+import java.util.*;
+
+public class Konyvtar {
+	public static void main(String[] args) {
+		
+		Scanner beolvasas = new Scanner(System.in);
+		Random veletlenszamobjektum = new Random();
+
+		int meret;
+		meret = 5;
+//		System.out.println("Hany darab konyvet szeretne tarolni?");
+//		System.out.println("darabszam:");
+//		meret = Integer.parseInt(beolvasas.nextLine());
+
+		Konyv konyvek[] = new Konyv[meret];
+
+		System.out.println("A konyvek adatai:");		
+		
+		for (int i = 0; i < konyvek.length; i++) {
+			/*
+			System.out.println("Kerem adja meg a(z) " + (i + 1) + ". konyv szerzojet!\nszerzo:");
+			String szerzo = beolvasas.nextLine();
+
+			System.out.println("Kerem adja meg a konyv cimet!\ncim:");
+			String cim = beolvasas.nextLine();
+
+			System.out.println("Kerem adja meg a konyv oldalszamat!\noldalszam:");
+			int oldalszam = beolvasas.nextInt();
+			
+			System.out.println("Kerem adja meg a konyv arat!\nar:");
+			int ar = beolvasas.nextInt();
+			//puffer uritese
+			beolvasas.nextLine();
+			
+			konyvek[i] = new Konyv(cim, szerzo, ar, oldalszam);
+			*/
+			
+			konyvek[i] = new Konyv("Cim" + (i + 1),
+					(char)(veletlenszamobjektum.nextInt('Z' - 'A' + 1) + 'A')
+					+ "" + (char)(veletlenszamobjektum.nextInt('Z' - 'A' + 1) + 'A'),
+					veletlenszamobjektum.nextInt(10000) + 1,
+					veletlenszamobjektum.nextInt(1500) + 1);
+			System.out.println(konyvek[i]);
+			
+		}
+
+		//Írja ki a leghosszabb könyv adatait!
+		Konyv leghosszabb_konyv = konyvek[0];
+		
+		for (int i = 1; i < konyvek.length; i++) {
+			leghosszabb_konyv = Konyv.hosszabbKonyv(konyvek[i], leghosszabb_konyv);			
+		}
+
+		System.out.println("A leghosszabb konyv adatai:");
+		System.out.println(leghosszabb_konyv);
+		
+		//Írja ki a leghosszabb páros oldalszámú könyv adatait!
+		Konyv leghosszabb_paros_oldalszamu_konyv = null;
+		for (int i = 0; i < konyvek.length; i++) {
+			if (konyvek[i].oldalszamParosE()) {
+				if (leghosszabb_paros_oldalszamu_konyv == null) {
+					leghosszabb_paros_oldalszamu_konyv = konyvek[i];
+				} else {
+					leghosszabb_paros_oldalszamu_konyv = Konyv.hosszabbKonyv(konyvek[i], leghosszabb_paros_oldalszamu_konyv);
+				}						
+			}
+		}
+
+		System.out.println("A leghosszabb paros oldalszamu konyv adatai:");
+		System.out.println(leghosszabb_paros_oldalszamu_konyv);
+		
+		//Írja ki, hogy melyik szerzõnek hány darab könyve jelent meg!
+		int darabszamok[] = new int[konyvek.length];
+		
+		for (int i = 0; i < konyvek.length; i++) {
+			String szerzo = konyvek[i].getSzerzo();
+
+			if (darabszamok[i] == 0) {
+				darabszamok[i]++;
+
+				for (int j = i + 1; j < konyvek.length; j++) {
+					if (szerzo.equals(konyvek[j].getSzerzo())) {
+						darabszamok[i]++;
+						darabszamok[j] = -1;
+					}				
+				}
+				
+				System.out.println(szerzo + "konyveinek darabszama: " + darabszamok[i]);			
+			}
+		}		
+		
+		beolvasas.close();
+	}
+}
