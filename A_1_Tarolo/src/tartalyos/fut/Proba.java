cf0cda0004df691d7389505e33eb2367991b231f
smidlaszlo <smid@iit.uni-miskolc.hu>
2018-05-03 11:09:43 +0200

    Tartaly, manualis adatbevitel

--- A_1_Tarolo/src/tartalyos/fut/Proba.java
+++ A_1_Tarolo/src/tartalyos/fut/Proba.java
@@ -8,24 +8,101 @@ Ebben hozzon létre 5 Tartaly objektumot beolvasott adatokkal!
  */
 package tartalyos.fut;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Random;
 
 import tartalyos.Tartaly;
 
 public class Proba {
 
+	public static final boolean 
+		MANUALIS_ADATBEVITEL = true;
+		//MANUALIS_ADATBEVITEL = false;	
+	
 	public static void main(String[] args) {
 		int meret = 5;
 		Tartaly[] tartalyok = new Tartaly[meret];
 		Random veletlenObjektum = new Random();
-
-		//TODO InputStreamReader/BufferedReader osztalyokkal is
+		BufferedReader beolvasas = new BufferedReader(new InputStreamReader(System.in));
+		int terfogat = 0;
+		double nyomas = 0;
 		
 		for (int i = 0; i < tartalyok.length; i++) {
-			tartalyok[i] = new Tartaly(veletlenObjektum.nextInt(20),
-					veletlenObjektum.nextDouble() * 20);
+			if (MANUALIS_ADATBEVITEL) {
+				
+				boolean rosszNyomas, rosszTerfogat;
+				rosszNyomas = rosszTerfogat = true;
+				
+				/*
+				do {
+					
+					if (rosszTerfogat) {
+						try {
+							System.out.println("Kerem adja meg a(z) " + (i + 1) + ". tartaly terfogatat!");
+							System.out.print("terfogat:");
+							terfogat = Integer.parseInt(beolvasas.readLine());
+							rosszTerfogat = false;
+						} catch (NumberFormatException e) {
+							System.out.println("Terfogatra nem egesz szam lett megadva!");
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+					}
+
+					if (rosszNyomas) {
+						try {
+							System.out.println("Kerem adja meg a(z) " + (i + 1) + ". tartaly nyomasat!");
+							System.out.print("nyomas:");
+							nyomas = Double.parseDouble(beolvasas.readLine());
+							rosszNyomas = false;
+						} catch (NumberFormatException e) {
+							System.out.println("Nyomasra nem valos szam lett megadva!");
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+					}					
+					
+				} while (rosszNyomas || rosszTerfogat);
+				*/
+
+				do {
+						try {
+							System.out.println("Kerem adja meg a(z) " + (i + 1) + ". tartaly terfogatat!");
+							System.out.print("terfogat:");
+							terfogat = Integer.parseInt(beolvasas.readLine());
+							rosszTerfogat = false;
+						} catch (NumberFormatException e) {
+							System.out.println("Terfogatra nem egesz szam lett megadva!");
+						} catch (IOException e) {
+							e.printStackTrace();
+						}					
+				} while (rosszTerfogat);
+
+				do {
+						try {
+							System.out.println("Kerem adja meg a(z) " + (i + 1) + ". tartaly nyomasat!");
+							System.out.print("nyomas:");
+							nyomas = Double.parseDouble(beolvasas.readLine());
+							rosszNyomas = false;
+						} catch (NumberFormatException e) {
+							System.out.println("Nyomasra nem valos szam lett megadva!");
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+				} while (rosszNyomas);				
+				
+				tartalyok[i] = new Tartaly(terfogat, nyomas);
+				
+			} else {//Nem MANUALIS_ADATBEVITEL, veletlenszamgeneralas
+				tartalyok[i] = new Tartaly(veletlenObjektum.nextInt(20),
+						veletlenObjektum.nextDouble() * 20);				
+			}
+			
 		}
 		
+		System.out.println();
 		System.out.println("A tartalyok adatai:");
 		for (Tartaly tartaly : tartalyok) {
 			System.out.println(tartaly);
